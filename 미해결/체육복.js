57479e96f3534136377e38ccbf8851d407a23499
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-09-29 21:19:45 +0900

    level1
    미완성문제  80점/100점
    
    testcase 공부해야함!

--- 미해결/체육복.js
+++ 미해결/체육복.js
@@ -23,7 +23,7 @@ function solution(n, lost, reserve) {
 }
 solution(n, lost, reserve);*/
 
-const n = 5;
+/*const n = 5;
 const lost = [2, 4];
 const reserve = [3];
 
@@ -41,4 +41,61 @@ function solution(n, lost, reserve) {
   return count > lost.length ? n : n - lost.length - count;
 }
 
+solution(n, lost, reserve);*/
+/*
+const n = 5;
+const lost = [2, 4];
+const reserve = [3];
+
+function solution(n, lost, reserve) {
+  const lost1 = lost.filter((item) => !reserve.includes(item)); //중복값제거한Lost 이때 reserve도 사라져야함
+  const lost2 = lost1.filter((item) => !reserve.includes(item + 1));
+  const lost3 = lost2.filter((item) => !reserve.includes(item - 1));
+
+  return n - lost3.length;
+}
+solution(n, lost, reserve);
+//arrA.filter(it => arrB.includes(it))*/
+const n = 5;
+const lost = [2, 4];
+const reserve = [1, 3, 5];
+
+function solution(n, lost, reserve) {
+  let T = lost.sort((a, b) => a - b);
+  let R = reserve.sort((a, b) => a - b);
+  for (i = 0; i < T.length; i++) {
+    for (let j = 0; j < R.length; j++) {
+      if (T[i] === R[j]) {
+        T.splice(i, 1);
+        R.splice(j, 1);
+        i = i - 1;
+        j = j - 1;
+      }
+    }
+  }
+
+  let M = T.map((item) => item + 1); //[1,3]
+  for (i = 0; i < M.length; i++) {
+    for (let j = 0; j < R.length; j++) {
+      if (M[i] === R[j]) {
+        M.splice(i, 1);
+        R.splice(j, 1);
+        i = i - 1;
+        j = j - 1;
+      }
+    }
+  }
+  let P = M.map((item) => item - 2);
+  for (i = 0; i < P.length; i++) {
+    for (let j = 0; j < R.length; j++) {
+      if (P[i] === R[j]) {
+        P.splice(i, 1);
+        R.splice(j, 1);
+        i = i - 1;
+        j = j - 1;
+      }
+    }
+  }
+  return n - P.length;
+}
 solution(n, lost, reserve);

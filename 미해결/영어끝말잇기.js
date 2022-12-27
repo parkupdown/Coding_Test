5a133353534dab11613a7f72e5bbd86c7414f54c
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-27 16:46:50 +0900

    level2 미해결 문제 해결

--- 미해결/영어끝말잇기.js
+++ 미해결/영어끝말잇기.js
@@ -1,68 +1,69 @@
-const n = 3;
-const words = [
-  "tank",
-  "kick",
-  "know",
-  "wheel",
-  "land",
-  "dream",
-  "mother",
-  "robot",
-  "tank",
-];
+const n = 2;
+const words = ["ac", "ca", "ac", "ac"];
+
+function findOneWord(n, words, i, answer) {
+  if (words[i].length < 2) {
+    const target = n - ((i + 1) % n);
+    const orderTarget = Math.ceil((i + 1) / n);
+    answer.push(target, orderTarget);
+
+    return true;
+  }
+}
+
+function findOverlap(n, words, i, answer, checkArr) {
+  if (checkArr.includes(words[i])) {
+    const target = n - ((i + 1) % n);
+    const orderTarget = Math.ceil((i + 1) / n);
+    answer.push(target, orderTarget);
+
+    return true;
+  }
+  checkArr.push(words[i]);
+}
+
+function findMatch(n, words, i, answer, checkArr) {
+  const splitWord = words[i].split("");
+  const lastIndex = splitWord.length - 1;
+
+  if (i !== 0 && splitWord[0] !== checkArr[i - 1]) {
+    const target = n - ((i + 1) % n);
+    const orderTarget = Math.ceil((i + 1) / n);
+    answer.push(target, orderTarget);
+
+    return true;
+  }
+
+  checkArr.push(splitWord[lastIndex]);
+}
+
 function solution(n, words) {
-  let A = words.map((item) => item.split("")).map((item) => [...item]);
-  let answer = [];
-  let answer2 = [];
-  let answer3 = [];
-  let answers = [];
-  let C = new Set(words);
-  let D = [...C];
+  const answer = [];
+  const checkArr = [];
 
-  if (D.length !== words.length) {
-    const Filterd = words.filter(
-      (item, index) => index !== words.indexOf(item)
-    );
-    const Index = words.lastIndexOf(Filterd[0]) + 1;
-    let NUM = Index % n === 0 ? n : Index % n;
-    answer2.push(NUM, Math.ceil(Index / n));
-  } //나누어 떨어지면 n 이반환 반복해서 말한거
-  for (i = 0; i < A.length - 1; i++) {
-    if (A[i].length === 1) {
-      let Num3 = (i + 1) / n === 0 ? n : (i + 1) % n;
-      let third = Math.ceil((i + 1) / n);
-      answer3.push(Num3, third);
-      break;
-    }
-
-    if (A[i][A[i].length - 1] !== A[i + 1][0]) {
-      let Num2 = (i + 1 + 1) % n === 0 ? n : (i + 1 + 1) % n;
-      //n+1이 배열 인덱스니까
-      //+1을 더해서 차례를 구한다. 그럼 5번째 차례
-      //엉뚱한거말한거
-      let second = Math.ceil((i + 1 + 1) / n);
-      answer.push(Num2, second);
-      break;
-    }
-  }
-  if (answer.length + answer2.length === 0) {
-    return [0, 0];
-  }
-  if (answer.length + answer2.length === 4) {
-    if (answer[1] > answer2[1]) {
-      return answer2;
-    }
-    if (answer[1] > answer2[1]) {
+  for (let i = 0; i < words.length; i++) {
+    if (findOneWord(n, words, i, answer, checkArr) === true) {
       return answer;
     }
-  }
-  if (answer.length + answer2.length === 2) {
-    if (answer[1] === undefined) {
-      return answer2;
-    } else {
+    if (findMatch(n, words, i, answer, checkArr) === true) {
+      return answer;
+    }
+    if (findOverlap(n, words, i, answer, checkArr) === true) {
       return answer;
     }
   }
 }
 solution(n, words);
-//쭉 가면서 그때 마다 검사를 해야한다!
+/* if (findOneWord(n, words, answer) === true) {
+    return answer;
+  }
+  if (findOverlap(n, words, answer) === true) {
+    return answer;
+  }
+  if (findMatch(n, words, answer) === true) {
+    return answer;
+  }
+  if (answer.length === 0) {
+    answer.push(0, 0);
+    return answer;
+  } */

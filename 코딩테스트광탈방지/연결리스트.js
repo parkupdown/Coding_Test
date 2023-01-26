7efc1760d556ef5c6337e895dfe54c6a3371cd6b
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-01-26 11:36:16 +0900

    연결리스트 학습_추가

---
+++ 코딩테스트광탈방지/연결리스트.js
@@ -0,0 +1,26 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class linkedList {
+  constructor() {
+    let init = new Node(`init`);
+    this.head = init;
+    this.tail = init;
+  }
+
+  append(data) {
+    const 새로운노드 = new Node(data);
+    this.tail.next = 새로운노드;
+    this.tail = 새로운노드;
+  }
+}
+
+const a = new linkedList();
+a.append(1);
+a.append(2);
+a.append(3);
+console.log(a);

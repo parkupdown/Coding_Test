bd2cced944c59aa270e58d750d1eb55071831d97
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-03-04 23:25:59 +0900

    refactor: 기능개발

--- Level2_Retry/기능개발.js
+++ Level2_Retry/기능개발.js
@@ -1,3 +1,67 @@
+const progresses = [95, 90, 99, 99, 80, 99];
+const speeds = [1, 1, 1, 1, 1, 1];
+function solution(progresses, speeds) {
+  const check = [];
+  let count = 0;
+  while (check.length < progresses.length) {
+    count = count + 1;
+    for (let i = 0; i < progresses.length; i++) {
+      progresses[i] = progresses[i] + speeds[i];
+      console.log(progresses);
+      if (progresses[i] >= 100 && check[i] === undefined) {
+        check[i] = count;
+      }
+      console.log(check);
+    }
+  }
+
+  const answer = [];
+  let target = check[0];
+  let answerCount = 1;
+  console.log(check);
+  for (let i = 1; i < check.length; i++) {
+    if (check[i] > target) {
+      target = check[i];
+      answer.push(answerCount);
+      answerCount = 1;
+    } else if (check[i] <= target) {
+      answerCount = answerCount + 1;
+    }
+  }
+  answer.push(answerCount);
+
+  return answer;
+}
+solution(progresses, speeds);
+/* const progresses = [93, 30, 55];
+const speeds = [1, 30, 5];
+
+function solution(progresses, speeds) {
+  let count = 0;
+  const answer = [];
+  while (progresses.length > 0) {
+    if (progresses[0] >= 100) {
+      progresses.shift();
+      count = count + 1;
+    }
+    if (progresses[0] < 100) {
+      if (count !== 0) {
+        answer.push(count);
+      }
+      count = 0;
+      for (let i = 0; i < progresses.length; i++) {
+        progresses[i] = progresses[i] + speeds[i];
+      }
+    }
+  }
+  if (count !== 0) {
+    answer.push(count);
+  }
+  return answer;
+}
+solution(progresses, speeds);
+*/
+/*
 let progresses = [93, 30, 55];
 let speeds = [1, 30, 5];
 function solution(progresses, speeds) {
@@ -17,3 +81,5 @@ function solution(progresses, speeds) {
   return answer;
 }
 solution(progresses, speeds);
+
+*/

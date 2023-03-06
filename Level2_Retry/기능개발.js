09bb16df8604d25c364c7077ebccbaba08c322dc
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-03-06 14:32:40 +0900

    refactor: level2 R 기능개발 100

--- Level2_Retry/기능개발.js
+++ Level2_Retry/기능개발.js
@@ -1,35 +1,19 @@
 const progresses = [95, 90, 99, 99, 80, 99];
 const speeds = [1, 1, 1, 1, 1, 1];
 function solution(progresses, speeds) {
-  const check = [];
-  let count = 0;
-  while (check.length < progresses.length) {
-    count = count + 1;
-    for (let i = 0; i < progresses.length; i++) {
-      progresses[i] = progresses[i] + speeds[i];
-      console.log(progresses);
-      if (progresses[i] >= 100 && check[i] === undefined) {
-        check[i] = count;
-      }
-      console.log(check);
-    }
-  }
-
   const answer = [];
-  let target = check[0];
-  let answerCount = 1;
-  console.log(check);
-  for (let i = 1; i < check.length; i++) {
-    if (check[i] > target) {
-      target = check[i];
-      answer.push(answerCount);
-      answerCount = 1;
-    } else if (check[i] <= target) {
-      answerCount = answerCount + 1;
+  const Day = progresses.map((item, index) => (100 - item) / speeds[index]);
+  let target = Day[0];
+  let count = 0;
+  for (let i = 0; i < Day.length; i++) {
+    if (Day[i] <= target) {
+      count = count + 1;
+    } else {
+      answer.push(count);
+      count = 1;
     }
   }
-  answer.push(answerCount);
-
+  answer.push(count);
   return answer;
 }
 solution(progresses, speeds);

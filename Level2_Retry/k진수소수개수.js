094967f1d5865b0b988624ea071ea80b626db8c2
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-03-07 22:04:35 +0900

    level2 : R k진수소수개수 해결

---
+++ Level2_Retry/k진수소수개수.js
@@ -0,0 +1,25 @@
+let n = 437674;
+let k = 3;
+
+function checkPrime(numbers) {
+  if (numbers < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(numbers); i++) {
+    if (numbers % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function solution(n, k) {
+  let answer = [];
+  n = n.toString(k).split("0");
+
+  n = n.filter((num) => checkPrime(num));
+
+  return n.length;
+}
+
+solution(n, k);

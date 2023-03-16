b147371875770bfbe9abf18278f283b3327f4a63
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-03-16 23:19:09 +0900

    level2 : 프렌즈4 블록 (kakao) 해결

---
+++ Level2_Retry/프렌즈4블록.js
@@ -0,0 +1,55 @@
+let m = 4;
+let n = 5;
+let board = ["CCBDE", "AAADE", "AAABF", "CCBBF"];
+
+function Find(arr, n, m, deleteArr) {
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < m - 1; j++) {
+      if (
+        arr[i][j] === arr[i][j + 1] &&
+        arr[i][j + 1] === arr[i + 1][j] &&
+        arr[i + 1][j] === arr[i + 1][j + 1] &&
+        arr[i][j] !== undefined
+      ) {
+        deleteArr.push([i, j]);
+      }
+    }
+  }
+  if (deleteArr.length === 0) {
+    return true;
+  }
+
+  deleteArr.forEach((item) => {
+    delete arr[item[0]][item[1]];
+    delete arr[item[0]][item[1] + 1];
+    delete arr[item[0] + 1][item[1]];
+    delete arr[item[0] + 1][item[1] + 1];
+  });
+}
+
+function solution(m, n, board) {
+  let deleteArr = [];
+  let count = 0;
+  board = board
+    .map((item) => item.split(``))
+    .reduce(
+      (result, row) => row.map((_, i) => [row[i], ...(result[i] || [])]),
+      []
+    );
+  console.log(board);
+  while (true) {
+    Find(board, n, m, deleteArr);
+    board = board.map((item) => item.filter((item2) => item2 !== undefined));
+    deleteArr = [];
+    if (Find(board, n, m, deleteArr) === true) {
+      break;
+    }
+  }
+
+  board.forEach((item) => (count = count + item.length));
+
+  return m * n - count;
+
+  //바꾸기 성공
+}
+solution(m, n, board);

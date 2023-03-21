3f902f7bc46f0a71ea456777948e5e0f116d7824
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-03-21 12:21:27 +0900

    study: 재귀함수 및 DFS, DP 학습

--- Level2_Retry/2xn타일링.js
+++ Level2_Retry/2xn타일링.js
@@ -6,3 +6,28 @@ function solution(n) {
 
   return arr[n];
 }
+let n = 5;
+
+function solution(n) {
+  var answer = 0;
+
+  // 타일을 채우는 방법의 가지 수
+  // 경우의 수가 많아 질 수 있으므로, 경우의 수를 1,000,000,007으로 나눈 나머지를 return해주세요.
+
+  search(0);
+
+  // DFS
+  function search(step) {
+    if (step === n) return answer++; // answer = (answer+1)%1000000007
+    if (step > n) return;
+    // 가로
+    console.log(step);
+    search(step + 2);
+    // 세로
+    console.log(step);
+    search(step + 1);
+  }
+
+  return answer; // answer%1000000007
+}
+solution(n);

05c5afcffe42be0174f4d022bdb13bd7b72ffb14
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-05-15 21:01:02 +0900

    retry: 롤케익자르기RE
    80점

--- Level2_Retry/롤케익자르기RE.js
+++ Level2_Retry/롤케익자르기RE.js
@@ -2,6 +2,58 @@ let topping = [1, 2, 1, 3, 1, 4, 1, 2];
 
 function solution(topping) {
   const stack = [];
+  //indexOf로 찾기
+  let count = 0;
+  let kindOfTopping = [...new Set(topping)];
+  let brotherHave = kindOfTopping.length;
+
+  kindOfTopping = kindOfTopping.map((item) => topping.lastIndexOf(item));
+
+  for (let i = 0; i < topping.length; i++) {
+    if (!stack.includes(topping[i])) {
+      stack.push(topping[i]);
+      // 철수가 가진거
+    }
+    if (kindOfTopping.includes(i)) {
+      brotherHave = brotherHave - 1;
+    }
+    //동생이 가진거 하나 뺏기
+
+    if (stack.length === brotherHave) {
+      count = count + 1;
+    }
+
+    if (stack.length > brotherHave) {
+      return count;
+    }
+  }
+  return count;
+}
+solution(topping);
+/*
+function solution(topping) {
+  const stack = [];
+  let count = 0;
+  for (let i = 0; i < topping.length; i++) {
+    const removeTopping = topping.pop();
+    if (!stack.includes(removeTopping)) {
+      stack.push(removeTopping);
+    }
+    const toppingTwo = [...new Set(topping)];
+
+    if (stack.length === toppingTwo.length) {
+      count = count + 1;
+    }
+    if (stack.length > toppingTwo) {
+      return count;
+    }
+  }
+  return count;
+}
+*/
+/*
+function solution(topping) {
+  const stack = [];
   let toppingSliced;
   let count = 0;
   for (let i = 0; i < topping.length; i++) {
@@ -19,3 +71,4 @@ function solution(topping) {
   return count;
 }
 solution(topping);
+*/

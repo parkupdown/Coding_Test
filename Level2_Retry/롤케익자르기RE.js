a16609dce650f0c7673c6b60bd45298d9acf8e05
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-05-15 21:02:16 +0900

    retry: 롤케익자르기RE
    pop()을 변수로 저장할 때는 pop이 먼저 실행되어 이를 이용해 includes메서드를 이용해서 마지막 요소인지 아닌지 알 수 있다.

--- Level2_Retry/롤케익자르기RE.js
+++ Level2_Retry/롤케익자르기RE.js
@@ -2,6 +2,31 @@ let topping = [1, 2, 1, 3, 1, 4, 1, 2];
 
 function solution(topping) {
   const stack = [];
+  let count = 0;
+  let you = [...new Set(topping)].length;
+  let me = 0;
+  while (true) {
+    const remove = topping.pop();
+    if (!stack.includes(remove)) {
+      stack.push(remove);
+      me++;
+    }
+    if (!topping.includes(remove)) {
+      you++;
+    }
+    if (you === me) {
+      count++;
+    }
+    if (you < me) {
+      return count;
+    }
+  }
+}
+solution(topping);
+
+/*
+function solution(topping) {
+  const stack = [];
   //indexOf로 찾기
   let count = 0;
   let kindOfTopping = [...new Set(topping)];
@@ -30,6 +55,7 @@ function solution(topping) {
   return count;
 }
 solution(topping);
+*/
 /*
 function solution(topping) {
   const stack = [];

eadfe49a08fa74a8643b6b9360512a80683e6e65
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-06-01 12:58:26 +0900

    retry: 고득점 키트 level1 k번째수

---
+++ Level2_Retry/k번째수.js
@@ -0,0 +1,10 @@
+function solution(array, commands) {
+  const answer = [];
+  for (let i = 0; i < commands.length; i++) {
+    const cutArray = array
+      .slice(commands[i][0] - 1, commands[i][1])
+      .sort((a, b) => a - b);
+    answer.push(cutArray[commands[i][2] - 1]);
+  }
+  return answer;
+}

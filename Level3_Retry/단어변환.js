89475bb576819d501eb10899c8eac15fb5d27eb6
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-07-17 13:54:54 +0900

    retry: 단어변환
    DFS와 BFS의 방법으로 각각 다르게 문제를 해결해봄

--- Level3_Retry/단어변환.js
+++ Level3_Retry/단어변환.js
@@ -31,3 +31,75 @@ function solution(begin, target, words) {
 
   return answer.length === 0 ? 0 : answer[0];
 }
+
+function solution(begin, target, words) {
+  //DFS풀이법
+  const check = (current, word) => {
+    let count = 0;
+    for (let i = 0; i < current.length; i++) {
+      if (current[i] !== word[i]) {
+        count = count + 1;
+      }
+    }
+    return count === 1 ? true : false;
+  };
+
+  const visited = Array.from({ length: words.length }, () => false);
+
+  let answer = 50;
+  const dfs = (current, count) => {
+    if (current === target) {
+      if (count < answer) {
+        answer = count;
+      }
+      return;
+    }
+    for (let i = 0; i < words.length; i++) {
+      if (!visited[i] && check(current, words[i])) {
+        visited[i] = true;
+        dfs(words[i], count + 1);
+        visited[i] = false;
+      }
+    }
+  };
+  dfs(begin, 0);
+
+  return answer === 50 ? 0 : answer;
+}
+// dfs로 다시풀어보기
+
+function solution(begin, target, words) {
+  //BFS풀이법
+  const check = (current, word) => {
+    let count = 0;
+    for (let i = 0; i < current.length; i++) {
+      if (current[i] !== word[i]) {
+        count = count + 1;
+      }
+    }
+    return count === 1 ? true : false;
+  };
+
+  const queue = [[begin]];
+  const visited = [];
+
+  let answer = 0;
+  while (queue.length > 0) {
+    for (let i = 0; i < queue.length; i++) {
+      const [current] = queue.shift();
+      if (current === target) {
+        return answer;
+      }
+      //이 단계에서 마다 방문처리가 되어져야함
+      for (let j = 0; j < words.length; j++) {
+        if (!visited.includes(j) && check(current, words[j])) {
+          visited.push(j);
+          queue.push([words[j]]);
+        }
+      }
+    }
+    answer = answer + 1;
+  }
+  return 0;
+}
+//bfs로해결

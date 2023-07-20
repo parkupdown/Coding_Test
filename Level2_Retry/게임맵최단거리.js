b1868dccdf1e8611d8d9828ab047c17c9778dbfb
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-07-20 11:41:21 +0900

    retry: 게임맵최단거리
    bfs로 100점으로 통과
    
    count를 매개변수로 넣어서 지나간 거리를 측정

--- Level2_Retry/게임맵최단거리.js
+++ Level2_Retry/게임맵최단거리.js
@@ -1,24 +1,42 @@
-const maps = [
-  [1, 0, 1, 1, 1],
-  [1, 0, 1, 0, 1],
-  [1, 0, 1, 1, 1],
-  [1, 1, 1, 0, 1],
-  [0, 0, 0, 0, 1],
-];
-
 function solution(maps) {
-  const bfs = (maps, position) => {
-    const checked = [];
-    const willCheck = [];
+  const visited = [];
 
-    willCheck.push(maps, position);
+  maps.forEach((item) => {
+    const arr = Array.from({ length: item.length }, () => false);
+    visited.push(arr);
+  });
 
-    while (willCheck.length !== 0) {
-      const node = willCheck.shift();
-      if (!checked.includes(node)) {
-        checked.push(node);
-        willCheck.push(maps);
+  // 4방향 모두 탐색해야하니까
+  const dx = [0, 1, 0, -1];
+  const dy = [1, 0, -1, 0];
+
+  const queue = [[0, 0, 1]];
+
+  visited[0][0] = true;
+
+  while (queue.length > 0) {
+    for (let i = 0; i < queue.length; i++) {
+      const [x, y, count] = queue.shift();
+      for (let j = 0; j < 4; j++) {
+        const nx = x + dx[j];
+        const ny = y + dy[j];
+
+        if (
+          nx >= 0 &&
+          ny >= 0 &&
+          nx < maps.length &&
+          ny < maps[0].length &&
+          maps[nx][ny] === 1 &&
+          !visited[nx][ny]
+        ) {
+          if (nx === maps.length - 1 && ny === maps[0].length - 1) {
+            return count + 1;
+          }
+          visited[nx][ny] = true;
+          queue.push([nx, ny, count + 1]);
+        }
       }
     }
-  };
+  }
+  return -1;
 }

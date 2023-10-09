22d227182a4d99f6328c061a1ad76105bb2f9f19
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-10-09 22:15:26 +0900

    retry: 베스트앨범(해시)

--- level3/베스트앨범.js
+++ level3/베스트앨범.js
@@ -31,3 +31,47 @@ function solution(genres, plays) {
   }
   return answer;
 }
+
+function solution(genres, plays) {
+  // 속한 노래가 많이 재생된 장르를 먼저 수록
+  // => hash이용해서 할 수 있을듯
+
+  // 장르 내에서 많이 재생된 노래를 먼저 수록
+  // => 그리고 해당 장르에서 또 가장 많은거순서로
+  // => 이건 이중for문으로 할 수 있을듯 가장 많이
+  // 최대 2개
+  // hash를 2번
+
+  // 장르 내에서 재생횟수가 같아면 고유번호가 낮은(앞에있는거부터)수록
+  const answer = [];
+  let genreHash = new Map();
+
+  for (let i = 0; i < genres.length; i++) {
+    genreHash.get(genres[i]) === undefined
+      ? genreHash.set(genres[i], plays[i])
+      : genreHash.set(genres[i], genreHash.get(genres[i]) + plays[i]);
+  }
+  // 많은 장르찾기
+
+  genreHash = [...genreHash].sort((a, b) => b[1] - a[1]);
+
+  for (let k = 0; k < genreHash.length; k++) {
+    const genre = genreHash[k][0];
+    let playHash = new Map();
+    for (let j = 0; j < genres.length; j++) {
+      if (genres[j] === genre) {
+        //만약 장르 종류가 우선순위 장르와 동일하고 아직 없다면 그때의 play횟수를 저장
+        playHash.set(j, plays[j]);
+      }
+    }
+    //여기서 이제 playHash를 다시 저장
+    playHash = [...playHash].sort((a, b) => b[1] - a[1]);
+    //만약 동일할경우
+    answer.push(playHash[0][0]);
+    if (playHash.length > 1) {
+      answer.push(playHash[1][0]);
+    }
+  }
+
+  return answer;
+}

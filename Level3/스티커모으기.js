30847b9d2e64f1754187554ed6cb20db50958d34
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-11-02 16:10:11 +0900

    refactor: 스티커모으기
    dp로 스스로 복습
    
    이해함

--- Level3/스티커모으기.js
+++ Level3/스티커모으기.js
@@ -204,3 +204,25 @@ function solution(sticker) {
 //dp로 해결했으니 블로그 참조
 
 //스스로 리펙토링 필요
+function solution(sticker) {
+  if (sticker.length === 1) {
+    return sticker[0];
+  }
+
+  const dp1 = Array.from({ length: sticker.length }, () => 0);
+  const dp2 = Array.from({ length: sticker.length }, () => 0);
+
+  dp1[0] = sticker[0];
+  dp1[1] = sticker[0];
+  dp2[1] = sticker[1];
+  //준비끝
+
+  for (let i = 2; i < sticker.length - 1; i++) {
+    dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + sticker[i]);
+  }
+  for (let i = 2; i < sticker.length; i++) {
+    dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + sticker[i]);
+  }
+
+  return Math.max(dp1[sticker.length - 2], dp2[sticker.length - 1]);
+}

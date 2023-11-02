8626538d7d8dd263567acc19582ec76d42cd38eb
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2023-11-02 14:34:35 +0900

    level3: 정수삼각형(dp)
    bottom - up 방식으로해결
    
    tip
    bottom - up 방식은 for문으로
    top - down 방식은 재귀로해결하는것이 일반적이다.

---
+++ Level3/정수삼각형.js
@@ -0,0 +1,37 @@
+function solution(triangle) {
+  // 정수삼각형
+
+  // 그니까 내려가면서 계속 그려나가는거지
+
+  for (let i = 0; i < triangle.length - 1; i++) {
+    let arr = Array.from({ length: i + 2 }, () => 0);
+    for (let j = 0; j < triangle[i].length; j++) {
+      const a = triangle[i][j] + triangle[i + 1][j];
+      const b = triangle[i][j] + triangle[i + 1][j + 1];
+
+      if (a > arr[j]) {
+        arr[j] = a;
+      }
+      if (b > arr[j + 1]) {
+        arr[j + 1] = b;
+      }
+    }
+    triangle[i + 1] = arr;
+  }
+
+  const lastLine = triangle[triangle.length - 1];
+
+  return Math.max(...lastLine);
+}
+//96점
+
+function solution(triangle) {
+  for (let i = triangle.length - 1; i > 0; i--) {
+    for (let j = 0; j < triangle[i].length - 1; j++) {
+      const max = Math.max(triangle[i][j], triangle[i][j + 1]);
+      triangle[i - 1][j] += max;
+    }
+  }
+
+  return triangle[0][0];
+}

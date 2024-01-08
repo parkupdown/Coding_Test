f785c314263153df4ce6e9da40f2c7f925b448e4
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2024-01-08 13:46:11 +0900

    level3: 연속펄스부분수열 (부분합)

--- 프로그래머스(학습)/Level3/연속펄스부분수열의합.js
+++ 프로그래머스(학습)/Level3/연속펄스부분수열의합.js
@@ -1,4 +1,42 @@
 function solution(sequence) {
+  const sum1 = sequence.map((item, index) =>
+    index % 2 === 0 ? item : item * -1
+  );
+  const sum2 = sequence.map((item, index) =>
+    index % 2 === 1 ? item : item * -1
+  );
+  const Length = sequence.length;
+  let answer1 = 0;
+  let answer2 = 0;
+  let stack1 = 0;
+  let stack2 = 0;
+  for (let i = 0; i < Length; i++) {
+    stack1 = stack1 + sum1[i];
+    if (stack1 < sum1[i]) {
+      //이게 더 커버리면 stack1 = sum1[i]
+      stack1 = sum1[i];
+    }
+    if (answer1 < stack1) {
+      answer1 = stack1;
+    }
+  }
+
+  for (let i = 0; i < Length; i++) {
+    stack2 = stack2 + sum2[i];
+    if (stack2 < sum2[i]) {
+      //이게 더 커버리면 stack1 = sum1[i]
+      stack2 = sum2[i];
+    }
+    //여기는 이제 현재까지의 stack, 즉 누적합의 최대값이 들어있음
+    if (answer2 < stack2) {
+      answer2 = stack2;
+    }
+  }
+  return Math.max(answer1, answer2);
+}
+// 테스트 통과
+
+function solution(sequence) {
   // 1또는 -1로 시작하면서 1과-1이 번갈아 나오는 수열의 가장 큰수
 
   // 연속해서 가장 큰 수
@@ -69,3 +107,39 @@ function solution(sequence) {
 
   return answer;
 }
+
+function solution(sequence) {
+  // 연속 펄스 부분 수열의 합
+
+  // 아 1 -1 1 -1 이나 -1 1 -1 1을 곱해서 최대가 되는 수열의 합
+
+  // 부호만 바뀌게 되는건데
+
+  // 근데 완탐으로 하면 시간이 너무 오래걸릴거 같은데
+  let answer = 0;
+  for (let i = 0; i < sequence.length; i++) {
+    // slice = > (0,2)이면 0,1까지 잘리는거
+    for (let j = 1; j <= sequence.length - i; j++) {
+      // 여기서 자름
+      const sliced = sequence.slice(i, i + j);
+
+      const sum1 = sliced.reduce(
+        (acc, cur, index) => (index % 2 === 1 ? acc + cur : acc + cur * -1),
+        0
+      );
+      const sum2 = sliced.reduce(
+        (acc, cur, index) => (index % 2 === 0 ? acc + cur : acc + cur * -1),
+        0
+      );
+
+      if (answer < sum1) {
+        answer = sum1;
+      }
+      if (answer < sum2) {
+        answer = sum2;
+      }
+    }
+  }
+  return answer;
+}
+// 효율성테스트 0점
